60d1f042d9ad1abc802dd6ccc22ade6d8ecfc29e
liusen <515882294@qq.com>
2024-09-07 04:49:51 +0800

    change redefine template

--- src/main/java/run/buddy/common/HelloWorldRedefine.java
+++ src/main/java/run/buddy/common/HelloWorldRedefine.java
@@ -3,17 +3,23 @@ package run.buddy.common;
 import lsieun.utils.OutputUtils;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
 
 public class HelloWorldRedefine {
     public static void main(String[] args) throws Exception {
         // 第一步，准备参数
         String className = "sample.HelloWorld";
+        Class<?> clazz = Class.forName(className);
 
 
         // 第二步，生成类
         ByteBuddy byteBuddy = new ByteBuddy();
-        DynamicType.Builder<?> builder = byteBuddy.redefine(Object.class)
+        DynamicType.Builder<?> builder = byteBuddy.redefine(clazz)
                 .name(className);
+
+        builder = builder.method(ElementMatchers.named("toString"))
+                .intercept(FixedValue.value("HelloToString"));
 
 
         // 第三步，输出结果
